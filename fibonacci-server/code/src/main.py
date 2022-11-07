aa20089a40b9129e996e2d800955c0b8eef71989
taiki <turkey.31452@gmail.com>
2022-11-07 17:00:11 +0900

    自動生成ドキュメントを追記

--- fibonacci-server/code/src/main.py
+++ fibonacci-server/code/src/main.py
@@ -28,8 +28,27 @@ def TooLargeResult_exception_handler(request: Request, exc: TooLargeResultExcept
     )
 
 
-@app.get("/fib")
-def read_item(n: int = Query(ge=0)):
+@app.get("/fib", tags=['Calculation'])
+    """
+    ### フィボナッチ数を返す
+
+    - **n**: フィボナッチ数列の順番を指定する値。10以上100000未満の整数を指定する。
+
+    ### 制約
+    - 処理の制約により、リクエストが100000以下でも、結果が4300桁を超える場合はエラーとなる。
+    <details>
+    <summary>エラーレスポンス</summary>
+    <code>
+    {
+    "detail": [
+        {
+        "msg": "The number of digits result exceeds 4300."
+        }
+    ]
+    }
+    </code>
+    </details>
+    """
     fib = fibonacci.fib(n)
     if fib != 0 and math.log10(fib) + 1 > 4300:
         raise TooLargeResultException(name=n)
